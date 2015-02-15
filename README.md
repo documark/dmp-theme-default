4db8271c534c59926b24455b86b8393e5043d1ac
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-02-15 01:36:13 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Documark Default Theme
+
+Default theme with handy plugins to get started right away!
+
+## Usage
+
+1. When in your `Documark` directory run:
+
+	```bash
+	$ npm init
+	$ npm install documark-theme-default --save
+	```
+
+2. Add the plugin to your `document.jade` front-matter:
+
+	```yaml
+	plugins:
+	  - documark-theme-default
+	```
---
+++ index.js
@@ -0,0 +1,29 @@
+var fs     = require('fs');
+var path   = require('path');
+var series = require('run-series');
+var stylus = require('stylus-renderer');
+
+module.exports = function themeDefault ($, document, cb) {
+	series([
+		function (next) {
+			// Add theme stylesheet
+			document.config().pdf.userStyleSheet = 'file://' + path.resolve(__dirname, 'assets/style.css');
+
+			stylus(path.join(__dirname, 'assets/style.styl'), {
+				stylusOptions: {
+					compress: ! document.config().debug
+				}
+			}, next);
+		},
+		function (next) {
+			document.applyPlugins($, [
+				require('documark-plugin-loader'),
+				require('documark-page-meta'),
+				require('documark-table-of-contents'),
+				require('documark-chapter-numbering'),
+				require('documark-relative-paths'),
+				require('documark-hr-to-page-break')
+			], next);
+		}
+	], cb);
+};
